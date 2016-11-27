dd4f84681c4af8346552b2cb99c56c50a7317e9b
IvMollov <mollov.i@abv.bg>
2016-11-27 18:28:31 +0200

    Add files via upload

---
+++ Problem-No.4b-Only-Two-Variables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_No._4b_Only_Two_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+            Console.WriteLine("\"a\" and \"b\" before the exchange:");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            b = a;
+            a = b + a;
+            Console.WriteLine("\"a\" and \"b\" after the exchange:");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+        }
+    }
+}
