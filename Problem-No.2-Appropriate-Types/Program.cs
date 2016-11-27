b789f508fbdd4d2e4c9e0149079ac4a870a59d6f
IvMollov <mollov.i@abv.bg>
2016-11-27 18:24:15 +0200

    Add files via upload

---
+++ Problem-No.2-Appropriate-Types/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_No._2_Appropriate_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 97;
+            sbyte b = -112;
+            short c = -10001;
+            ushort d = 44530;
+            int e = 5734821;
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("c = " + c);
+            Console.WriteLine("d = " + d);
+            Console.WriteLine("e = " + e);
+        }
+    }
+}
