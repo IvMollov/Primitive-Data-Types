32f29f2f536a2bd1bb9b2369162a66b21116f526
IvMollov <mollov.i@abv.bg>
2016-11-27 18:31:26 +0200

    Add files via upload

---
+++ Problem-No.7-Multiples/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_No._7_Multiples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int three = 3;
+            int five = 5;
+            int sum = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (three * i < 1000)
+                {   
+                    sum += (three * i);
+                }
+                if (five * i < 1000)
+                {  
+                    sum += (five * i);
+                }
+            }
+            Console.WriteLine("Sum of all the multiples of 3 and 5 below 1000 is: {0}", sum);
+            Console.ReadLine();
+
+        }
+    }
+}
