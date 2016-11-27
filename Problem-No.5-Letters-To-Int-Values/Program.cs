ae895dad4d8a2020c68a87ad74900aa90db1d4aa
IvMollov <mollov.i@abv.bg>
2016-11-27 18:29:26 +0200

    Add files via upload

---
+++ Problem-No.5-Letters-To-Int-Values/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_No._5_letters_to_int_values
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = (int)'a'; i <= (int)'z'; i++)
+            {
+                Console.WriteLine("Letter \"{0}\" has integer value of {1}", (char)i, i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
