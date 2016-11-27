466dfc7e0bf744ea9abe5c285568f4fc506b2224
IvMollov <mollov.i@abv.bg>
2016-11-27 18:27:00 +0200

    Add files via upload

---
+++ Problem-No.4a-Integer-Variables/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_No._4a_integer_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {      
+            int a = 5;
+            int b = 10;
+            int c;
+            Console.WriteLine("\"a\" and \"b\" before the exchange:");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("\"a\" and \"b\" after the exchange:");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+        }
+    }
+}
